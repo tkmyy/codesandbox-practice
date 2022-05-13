6cb41f1ea2bdbd2ef49782b3966c3fd33be9660d
takumi <55521780+tkmyy@users.noreply.github.com>
2022-05-13 23:39:14 +0900

    operators

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 /**
- * mapやfilterを使った配列の処理
+ * 三項演算子
  */
-const nameArr = ["tanaka", "yamada", "sato"];
-for (let index = 0; index < nameArr.length; index++) {
-  console.log(nameArr[index]);
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+const val1 = 1 > 0 ? "trueです" : "falseです";
+console.log(val1);
+
+const num1 = 1300;
+console.log(num1.toLocaleString());
+
+const num2 = "1300";
+const formattedNum =
+  typeof num2 === "number" ? num2.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
+console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+const flag1 = true;
+const flag2 = false;
+
+if (flag1 || flag2) {
+  console.log("1か2はtrueになります");
+}
+if (flag1 && flag2) {
+  console.log("1か2はtrueになります");
 }
 
-const nameArr2 = nameArr.map((name) => {
-  return name;
-});
-console.log(nameArr2);
+// || は左側がfalseなら右側を返す
+const num3 = null;
+const fee = num3 || "金額未設定です";
+console.log(fee);
 
-nameArr.map((name) => console.log(name));
-nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
-
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
-
-const newNameArr = nameArr.map((name) => {
-  if (name === "sato") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// && は左側がtrueなら右側が返す
+const num4 = 100;
+const fee2 = num4 && "何か設定されました";
+console.log(fee2);
