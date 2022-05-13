145699b1eacfa96fc3d80612de9bce9a66016820
takumi <55521780+tkmyy@users.noreply.github.com>
2022-05-13 22:49:27 +0900

    spread syntax

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 /**
- * 分割代入
+ * スプレッド構文 ...
  */
-const myProfile = {
-  name: "takumi",
-  age: 24
-};
+// 配列の展開
+const arr1 = [1, 2];
+console.log(arr1);
+console.log(...arr1);
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-console.log(message1);
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
 
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
 
-// const myProfile = ["takumi", 24];
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
 
-// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-// console.log(message3);
+const arr6 = [...arr4];
+console.log(arr6);
 
-// const [name, age] = myProfile;
-// const message4 = `名前は${name}です。年齢は${age}歳です。`;
-// console.log(message4);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
 
-/**
- * デフォルト値、引数など
- */
-const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
-sayHello("takumi");
-sayHello();
+const arr8 = arr4;
+console.log(arr8);
+
+arr8[0] = 100;
+console.log(arr8);
+console.log(arr4);
+
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
