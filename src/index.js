454312cf23ce21913b766e9639535c5f1ad6a3b4
takumi <55521780+tkmyy@users.noreply.github.com>
2022-05-13 22:06:02 +0900

    temp string and arrow func

--- src/index.js
+++ src/index.js
@@ -1,46 +1,37 @@
 /**
- * const, let等の変数宣言
+ * テンプレート文字列
  */
-var val1 = "var変数";
-console.log(val1);
+const name = "takumi";
+const age = 24;
+// 「私の名前はtakumiです。年齢は24歳です。」
 
-// varh変数は上書き可能
-val1 = "var変数を上書き";
-console.log(val1);
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(message1);
 
-// var変数は再宣言可能
-// var val1 = "var変数を再宣言";
-// console.log(val1);
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
 
-let val2 = "let変数";
-console.log(val2);
-
-// letは上書きが可能
-val2 = "let変数を上書き";
-console.log(val2);
-
-// letは再宣言不可能
-//let val2 = "let変数を再宣言";
-
-const val3 = "const変数";
-console.log(val3);
-
-// const変数は上書き不可
-// val3 = "const変数を上書き";
-
-// const val3 = "const変数を再宣言";
-
-// constで定義したオブジェクトはプロパティの変更が可能
-const val4 = {
-  name: "takumi",
-  age: 24
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
 };
-val4.name = "TAKUMI";
-val4.address = "Tokyo";
-console.log(val4);
+console.log(func1("func1です"));
 
-// constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
